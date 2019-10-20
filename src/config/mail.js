05caabcd5baccb86e3f49d28b2628e8beac6c0f2
Everton <vertopge@hotmail.com>
2019-10-20 19:22:32 -0300

    feat: send email on appointment cancellation

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c32aee358e820f',
+    pass: '3e027e6c4e4900',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
